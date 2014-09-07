3ec620c8c965f184ebbfc219447843198565ee2e
jortenberg <jill.ortenberg@gmail.com>
2014-09-07 15:10:56 -0400

    in the weeds with edit-puts-downvote

--- server.rb
+++ server.rb
@@ -18,16 +18,21 @@ get("/") do #shows all posts chron backwards
   erb(:index, { locals: { categories: Category.all(), posts: Post.all() } })
 end
 
+get("/categroies/:id/posts") do #shows all posts chron backwards in 1 category
+  # Post.limit(10).order(asc)
+  # Post.limit(10).last
+
+  posts = Post.where(:category_id => params["category.id"])
+
+  erb(:posts_by_category, { locals: { categories: Category.all(), posts: posts } })
+end
+
 get("/posts/:id") do #shows one post
   post = Post.find_by({id: params["id"]})
 
   comments = Comment.where(:post_id => params["id"])
 
   erb(:post_show, { locals: { post: post, comments: comments, categories: Category.all() } })
-end
-
-get("/categories/:id/posts") do #shows posts from one category chron backwards
-  erb(:posts_by_category, { locals: { categories: Category.all(), posts: Post.all() } })
 end
 
 get("/categories/new") do #shows form to make new Category
@@ -75,7 +80,9 @@ post("/posts/new") do
 
   post = Post.find_by({quote: params["quote"]})
 
-  erb(:post_show, { locals: { post: post, categories: Category.all(), posts: Post.all() } }) #after someone posts a new post they see a page of just their post alone.
+  comments = Comment.where(:post_id => post.id)
+
+  erb(:post_show, { locals: { post: post, comments: comments, categories: Category.all() } }) #after someone posts a new post they see a page of just their post alone.
 end
 
 post("/posts/comment") do #post comment from form on the post_show.erb page
@@ -93,28 +100,29 @@ post("/posts/comment") do #post comment from form on the post_show.erb page
   erb(:post_show, { locals: { post: post, comments: comments, categories: Category.all() } })
 end
 
-post("/posts/thumbdown/") do
+put("/posts/thumbsdown/:id") do #This does not work!!!!!
+  binding.pry
+
+  increment = params["downvotes"] + 1
+
+  post_hash = { 
+    author: params["author"],
+    source: params["source"],
+    posted_by: params["posted_by"],
+    quote: params["quote"],
+    expires_on: params["expires_on"],
+    downvotes: increment,
+    upvotes: params["upvotes"],
+    category_id: params["category_id"]  
+  }
 
   post = Post.find_by({id: params["id"]})
+  post.update(post_hash)
 
- #does it need to be a puts edit to do this????
-  post.downvotes += 1
+  comments = Comment.where(:post_id => params["id"])
 
-  erb(:post_show, { locals: { post: post, categories: Category.all(), posts: Post.all(), comments: Comment.all() } })
+  erb(:post_show, { locals: { post: post, comments: comments, categories: Category.all() } })
 end
-
-# put("/characters/:id") do
-#   character_hash = { 
-#     name: params["name"], 
-#     image_url: params["url"], 
-#     house_id: params["house_id"]
-#   }
-
-#   character = Character.find_by({id: params[:id]})
-#   character.update(character_hash)
-
-#   erb(:"characters/show", { locals: { character: character } })
-# end
 
 
 
